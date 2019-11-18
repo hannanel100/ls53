0c98a133f863879e208781ebcb67cacd9c4b26c0
Hannanel Gershinsky <hannanel100@gmail.com>
2019-11-18 22:24:53 +0200

    i recieve token from server and save in localstorage

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
+
 const endPoint = "http://localhost:3201/car";
 let tableBody = document.getElementById('table-body');
 fetch(endPoint)
-    .then((res) =>res.json()
-    .then((data)=> tableBuild(data)))
+    .then((res) => res.json()
+        .then((data) => tableBuild(data)))
     .catch(err => console.log(err));
 
 const tableBuild = (carsArray) => {
     let table = '';
-    for(let i = 0; i < carsArray.length; i++){
+    for (let i = 0; i < carsArray.length; i++) {
         table += `<tr>
                     <td>${carsArray[i].id}</td>
                     <td>${carsArray[i].name}</td>
@@ -21,8 +22,26 @@ const tableBuild = (carsArray) => {
     tableBody.innerHTML = table;
 }
 
-let user = document.getElementById('username');
-user.addEventListener("change", function(e){
+
+const loginBtn = document.getElementById('login-btn')
+loginBtn.addEventListener("click", function (e) {
     //GET INPUT OF USERNAME
+    const params = {
+        user: document.getElementById('user').value,
+        pass: document.getElementById('pass').value
+    };
+    console.log(params);
+    fetch('http://localhost:3201/auth', {
+        method: 'POST',
+        body: JSON.stringify(),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'basic ' + btoa(params.user + ':' + params.pass)
+        }
+    }).then(res => {
+        delete params;
+        res.text().then(token => {
+            window.localStorage.setItem('token', token);
+        })
+    })
 })
-const  
--- server.js
+++ server.js
@@ -10,20 +10,50 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
-// app.use(function (req, res, next) {
-//     if (req.method === 'POST' && req.path === '/auth') {
-//         next();
-//     }
-//     else {
-//         try {
-//             const token = jwt.verify(splitCredentials(req.headers.authorization), 'shhhhh');
-//             next();
-//         } catch (ex) {
-//             console.log(ex);
-//             res.status(403).send();
-//         }
-//     }
-// })
+app.use(function (req, res, next) {
+    if (req.method === 'POST' && req.path === '/auth') {
+        next();
+    }
+    else {
+        try {
+            const token = jwt.verify(splitCredentials(req.headers.authorization), 'shhhhh');
+            next();
+        } catch (ex) {
+            console.log(ex);
+            res.status(403).send();
+        }
+    }
+})
+app.post('/auth', function (req, res) {
+    const { user, pass } = splitCredentials(req.headers.authorization);
+    if (!user) {
+        res.status(403).send();
+    } else {
+        if (user === 'student' && pass === 'pass123') {
+            const token = jwt.sign({
+                user: user,
+            }, 'shoko',
+                {
+                    expiresIn: 500
+                });
+            res.send(token);
+        } else {
+            res.status(403).send();
+        }
+    }
+});
+
+function splitCredentials(str) {
+    const authHeader = str.split(' ');
+    if (authHeader[0] === 'basic') {
+        return {
+            user: atob(authHeader[1]).split(':')[0],
+            pass: atob(authHeader[1]).split(':')[1]
+        }
+    } else if (authHeader[0] === 'bearer') {
+        return authHeader[1];
+    }
+}
 app.get('/car', (req, res) => {
     carBl.getCars((e, data) => {
         if (e) {
